7ca6286b86ab1d5e2282d4c83c4dd3b925c97bfb
dave <nds95@kakao.com>
2024-11-25 23:09:59 +0900

    add project

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  height: 50px;
-  padding: 10px 10px;
-  color: blue;
-`
 
 function App() {
   return (
     <div className="App">
-      <Container>nav</Container>
-      <div>body</div>
-      <div>bottom</div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
