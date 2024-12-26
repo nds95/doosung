90cb7d1d337e00886ced547c985216f3c939b33c
Dave <nds95@kakao.com>
2024-12-26 22:10:13 +0900

    add vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@headers", replacement: "/src/components/headers" },
+      { find: "@pages", replacement: "/src/pages" },
+    ],
+  },
+});
